6f432a945e6395d3e27fca4e0622faa49f30fb8d
littlebookboy <kevin2pig@gmail.com>
2021-06-27 12:04:10 +0800

    refactor

--- app/Services/StringCalculatorService.php
+++ app/Services/StringCalculatorService.php
@@ -11,13 +11,34 @@ class StringCalculatorService
     //
     public function add(string $numbers): int
     {
-        if (!empty($numbers)) {
-            if (strpos($numbers, ',') !== false) {
-                $numbers = explode(',', $numbers);
-                return $numbers[0] + $numbers[1];
-            }
-            return $numbers;
+        if (empty($numbers)) {
+            return 0;
         }
-        return 0;
+
+        if ($this->isTwoNumbers($numbers)) {
+            $numbers = $this->splitNumbers($numbers);
+            return $numbers[0] + $numbers[1];
+        }
+
+        return $numbers;
+    }
+
+    /**
+     * @param string $numbers
+     * @return bool
+     */
+    public function isTwoNumbers(string $numbers): bool
+    {
+        return strpos($numbers, ',') !== false;
+    }
+
+    /**
+     * @param string $numbers
+     * @return false|string[]
+     */
+    public function splitNumbers(string $numbers)
+    {
+        $numbers = explode(',', $numbers);
+        return $numbers;
     }
 }
