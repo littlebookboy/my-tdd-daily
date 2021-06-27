ee57ca16a9ae10243de9b37c9a4316a126f7436c
littlebookboy <kevin2pig@gmail.com>
2021-06-27 11:29:24 +0800

    second red light: test add one nunmber

--- tests/Services/StringCalculatorServiceTest.php
+++ tests/Services/StringCalculatorServiceTest.php
@@ -7,11 +7,24 @@ use PHPUnit\Framework\TestCase;
 
 class StringCalculatorServiceTest extends TestCase
 {
+    private $stringCalculatorService;
+
     public function test_add_empty_string_got_zero()
     {
-        $stringCalculatorService = new StringCalculatorService();
-        $sum = $stringCalculatorService->add('');
+        $sum = $this->stringCalculatorService->add('');
         $this->assertEquals(0, $sum);
     }
 
+
+    public function test_add_one_got_one()
+    {
+        $sum = $this->stringCalculatorService->add('1');
+        $this->assertEquals(1, $sum);
+    }
+
+
+    protected function setUp(): void
+    {
+        $this->stringCalculatorService = new StringCalculatorService();
+    }
 }
