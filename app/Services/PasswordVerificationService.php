9023352f6485a7da5093e73b499301ca00e60505
littlebookboy <kevin2pig@gmail.com>
2021-08-07 21:32:32 +0800

    refactor

--- app/Services/PasswordVerificationService.php
+++ app/Services/PasswordVerificationService.php
@@ -9,24 +9,35 @@ use Exception;
 class PasswordVerificationService
 {
     /**
-     * @param string $password
+     * @var string $password
      */
-    public function verify($password)
+    private $password;
+
+    /**
+     * PasswordVerificationService constructor.
+     * @param $password
+     */
+    public function __construct($password)
     {
-        throw_if(is_null($password), Exception::class, 'password should not be null');
-        throw_if(strlen($password) < 8, Exception::class, 'password should be larger than 8 chars');
+        $this->password = $password;
+    }
+
+    public function verify()
+    {
+        throw_if(is_null($this->password), Exception::class, 'password should not be null');
+        throw_if(strlen($this->password) < 8, Exception::class, 'password should be larger than 8 chars');
         throw_if(
-            !preg_match('/[A-Z]/', $password),
+            !preg_match('/[A-Z]/', $this->password),
             Exception::class,
             'password should have one uppercase letter at least'
         );
         throw_if(
-            !preg_match('/[a-z]/', $password),
+            !preg_match('/[a-z]/', $this->password),
             Exception::class,
             'password should have one lowercase letter at least'
         );
         throw_if(
-            !preg_match('/[0-9]/', $password),
+            !preg_match('/[0-9]/', $this->password),
             Exception::class,
             'password should have one number at least'
         );
